00cc031e0aa1286a234d40bf20c0004546fa9449
Alexandre Mathieu <73568394+alexmathieu22@users.noreply.github.com>
2022-01-16 13:16:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Floppa
+
+Le meilleur site de vente anonyme au Québec!
+
+## Requis
+
+- Java 11
+- Maven
+
+## Setup
+
+### Compilation
+
+```
+mvn clean install
+```
+
+### Exécution
+
+```
+mvn exec:java
+```
---
+++ src/main/java/ulaval/glo2003/Main.java
@@ -0,0 +1,20 @@
+package ulaval.glo2003;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        ResourceConfig resourceConfig = new ResourceConfig()
+                .packages("ulaval.glo2003");
+        URI uri = URI.create("http://localhost:8080/");
+
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
+        server.start();
+    }
+}
