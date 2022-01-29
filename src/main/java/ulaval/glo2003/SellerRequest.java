7735c89cc6bbf411992c0bc50d7cc4bd6cbdca37
Simon <simonhoude2406@gmail.com>
2022-01-29 11:12:23 -0500

    Create seller object and store it

--- src/main/java/ulaval/glo2003/SellerRequest.java
+++ src/main/java/ulaval/glo2003/SellerRequest.java
@@ -1,9 +1,10 @@
 package ulaval.glo2003;
 
+import java.time.OffsetDateTime;
 import java.util.Date;
 
 public class SellerRequest {
     public String name;
     public String bio;
-    public Date birthDate;
+    public OffsetDateTime birthDate;
 }
