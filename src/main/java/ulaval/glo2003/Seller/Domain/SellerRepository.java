a281900f2df4d71fc0dd6506823b5f1de1c852e1
Alexandre Mathieu <alexandre.mathieu.7@ulaval.ca>
2022-02-18 18:46:51 -0500

    Add seller & product repository

---
+++ src/main/java/ulaval/glo2003/Seller/Domain/SellerRepository.java
@@ -0,0 +1,23 @@
+package ulaval.glo2003.Seller.Domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class SellerRepository {
+    private final Map<String, Seller> sellers;
+
+    public SellerRepository() {
+        sellers = new HashMap<>();
+    }
+
+    public Optional<Seller> find(String sellerId) {
+        return Optional.ofNullable(sellers.get(sellerId));
+    }
+
+    public void save(Seller seller) {
+        String sellerId = seller.getId();
+        sellers.put(sellerId, seller);
+    }
+}
+
