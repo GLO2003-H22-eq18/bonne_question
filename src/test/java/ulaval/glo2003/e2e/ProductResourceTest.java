0abc3307da809ea19fad8ada4f67bb9d4b3e2bd3
Simon Houde <simon.houde.2@ulaval.ca>
2022-02-24 09:02:51 -0500

    Add methods template for tests of product ressource

--- src/test/java/ulaval/glo2003/e2e/ProductResourceTest.java
+++ src/test/java/ulaval/glo2003/e2e/ProductResourceTest.java
@@ -1,8 +1,11 @@
 package ulaval.glo2003.e2e;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import ulaval.glo2003.Main;
+import ulaval.glo2003.Product.UI.ProductRequest;
 import ulaval.glo2003.Seller.Domain.Seller;
 import ulaval.glo2003.Seller.UI.SellerRequest;
 
@@ -11,6 +14,8 @@ import java.time.Clock;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+
+//import static spark.Spark.stop; TODO
 
 public class ProductResourceTest {
 
@@ -24,12 +29,57 @@ public class ProductResourceTest {
 
     @BeforeEach
     public void createDefaultSeller(){
-        defaultSeller = new Seller("John Cena",
+        Seller defaultSeller = new Seller("John Cena",
                     "What a chad!",
                     OffsetDateTime.now(Clock.systemUTC()),
                     LocalDate.parse("1977-04-23"),
                     new ArrayList<>());
     }
+
+    @AfterAll
+    static void tearDownServer() {
+        //stop();  TODO NO NECESSARY?
+    }
+
+    @Test
+    void givenProduct_whenMakingPOSTRequestToProductEndpoint_thenCorrect(){
+
+
+        ArrayList<String> listOfCategories = new ArrayList<>();
+
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.title = "";
+        productRequest.description = "";
+        productRequest.suggestedPrice = 5.0;
+        productRequest.categories = new ArrayList<>();
+    }
+
+
+    @Test
+    void givenProduct_whenMakingPOSTRequestToProductEndpointWithMissingField_thenMissingParameterError() {
+        //TODO
+    }
+
+    @Test
+    void givenProduct_whenMakingPOSTRequestToProductEndpointWithInvalidField_thenInvalidParameterError() {
+        //TODO
+    }
+
+    @Test
+    void whenMakingGETResquestToProductEndpoint_thenCorrect(){
+        //TODO 200
+    }
+
+    @Test
+    void whenMakingGETRequestToProductEndpointWithAbsentProductId_thenItemNotFoundError() {
+        //TODO
+    }
+
+    @Test
+    void whenMakingGETResquestToProductEndpointWithFilter_thenCorrect(){
+        //TODO 200
+    }
+
 
 
 
