4cc9adbfab22fed91308eeb4b8105e9566fdd813
Kevin Jobin <kevin.jobin.1@ulaval.ca>
2022-02-25 15:16:47 -0500

    Modify End2End tests to extract response object

--- src/test/java/ulaval/glo2003/e2e/End2EndTests.java
+++ src/test/java/ulaval/glo2003/e2e/End2EndTests.java
@@ -1,31 +1,53 @@
 package ulaval.glo2003.e2e;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-import io.restassured.http.Header;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import ulaval.glo2003.Main;
+import ulaval.glo2003.Exceptions.InvalidArgumentExceptionMapper;
+import ulaval.glo2003.Exceptions.ItemNotFoundExceptionMapper;
+import ulaval.glo2003.Exceptions.MissingArgumentExceptionMapper;
+import ulaval.glo2003.Product.Domain.ProductFactory;
+import ulaval.glo2003.Product.Domain.ProductRepository;
 import ulaval.glo2003.Product.UI.ProductRequest;
+import ulaval.glo2003.Product.UI.ProductResource;
+import ulaval.glo2003.Seller.Domain.SellerFactory;
+import ulaval.glo2003.Seller.Domain.SellerRepository;
 import ulaval.glo2003.Seller.UI.SellerRequest;
+import ulaval.glo2003.Seller.UI.SellerResource;
 
 import java.io.IOException;
+import java.net.URI;
 
 import static com.google.common.truth.Truth.assertThat;
-import static io.restassured.RestAssured.given;
 import static ulaval.glo2003.e2e.End2EndUtils.*;
 
 
 class End2EndTests {
 
     @BeforeAll
-    public static void setup() throws IOException {
-        Main.main(new String[] {});
+    public static void startServer() throws IOException {
+        SellerRepository sellerRepository = new SellerRepository();
+        SellerFactory sellerFactory = new SellerFactory();
+        SellerResource sellerResource = new SellerResource(sellerRepository, sellerFactory);
+
+        ProductRepository productRepository = new ProductRepository();
+        ProductFactory productFactory = new ProductFactory(sellerRepository);
+        ProductResource productResource = new ProductResource(productRepository, productFactory);
+
+        ResourceConfig resourceConfig = new ResourceConfig()
+                .register(sellerResource)
+                .register(productResource)
+                .register(new InvalidArgumentExceptionMapper())
+                .register(new MissingArgumentExceptionMapper())
+                .register(new ItemNotFoundExceptionMapper())
+                .packages("ulaval.glo2003");
+        URI uri = URI.create("http://localhost:8080/");
+
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
+        server.start();
     }
 
     @Test
@@ -36,21 +58,55 @@ class End2EndTests {
     }
 
     @Test
-    void canCreateSellerHasLocationHeader() {
+    void givenSeller_whenCreatingSeller_thenExist() {
         SellerRequest sellerRequest = createValidSeller();
 
-        ExtractableResponse<Response> response = createSellerResource(sellerRequest);
+        Response response = createSellerResource(sellerRequest);
 
         assertThat(response.statusCode()).isEqualTo(201);
+        assertThat(response.body()).isNotNull();
         assertThat(extractLocationId(response)).isNotNull();
         assertThat(extractLocationId(response)).isNotEmpty();
+    }
+
+    @Test
+    void givenProduct_whenCreatingProduct_thenExist() {
+        ProductRequest productRequest = createValidProduct();
+
+        Response response = createProductResource(productRequest, createSellerGetId());
+
+        assertThat(response.statusCode()).isEqualTo(201);
+        assertThat(response.body()).isNotNull();
+        assertThat(extractLocationId(response)).isNotNull();
+        assertThat(extractLocationId(response)).isNotEmpty();
+    }
+
+    @Test
+    void givenProductId_whenGettingProduct_thenProductReturned() throws Exception {
+        String productId = createProductGetId();
+
+        Response response = getProductById(productId);
+
+        assertThat(response.statusCode()).isEqualTo(200);
+    }
+
+    @Test
+    void givenSellerId_whenGettingSeller_thenSellerReturnedWithProduct() throws Exception {
+        String sellerId = createSellerGetId();
+        String productId = addProductToSellerGetId(sellerId);
+
+        Response response = getSellerById(sellerId);
+
+        System.out.println(response);
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.body()).isNotNull();
     }
 
     @Test
     void givenSellerRequestWithMissingParam_whenCreatingSeller_thenError400() {
         SellerRequest sellerRequest = createMissingParamSeller();
 
-        ExtractableResponse<Response> response = createSellerResource(sellerRequest);
+        Response response = createSellerResource(sellerRequest);
 
         assertThat(response.statusCode()).isEqualTo(400);
         assertThat(response.body()).isNotNull();
@@ -60,21 +116,12 @@ class End2EndTests {
     void givenSellerRequestWithInvalidParam_whenCreatingSeller_thenError400() {
         SellerRequest sellerRequest = createInvalidParamSeller();
 
-        ExtractableResponse<Response> response = createSellerResource(sellerRequest);
+        Response response = createSellerResource(sellerRequest);
 
         assertThat(response.statusCode()).isEqualTo(400);
         assertThat(response.body()).isNotNull();
     }
 
-    @Test
-    void canCreateProductIsAddedToSeller() {
-        ProductRequest productRequest = createProduct();
 
-        ExtractableResponse<Response> response = createProductResource(productRequest, createSellerGetId());
-
-        assertThat(response.statusCode()).isEqualTo(201);
-        assertThat(extractLocationId(response)).isNotNull();
-        assertThat(extractLocationId(response)).isNotEmpty();
-    }
 
 }
--- src/test/java/ulaval/glo2003/e2e/End2EndUtils.java
+++ src/test/java/ulaval/glo2003/e2e/End2EndUtils.java
@@ -1,20 +1,22 @@
 package ulaval.glo2003.e2e;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
-import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import ulaval.glo2003.Product.UI.ProductRequest;
-import ulaval.glo2003.Seller.UI.SellerRequest;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import ulaval.glo2003.Product.UI.ProductRequest;
+import ulaval.glo2003.Seller.UI.SellerRequest;
 
 import static io.restassured.RestAssured.*;
 
 public class End2EndUtils {
 
     private static final String SELLER_HEADER_NAME = "X-Seller-Id";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static int getHealth(){
         return given().contentType(ContentType.JSON)
@@ -50,15 +52,26 @@ public class End2EndUtils {
 
     public static String createSellerGetId(){
         SellerRequest sellerRequest = createValidSeller();
-        ExtractableResponse<Response> response = createResource("/sellers", sellerRequest);
-        return response.header("Location").split("/")[4];
+        Response response = createResource("/sellers", sellerRequest);
+        return extractLocationId(response);
     }
 
-    public static ExtractableResponse<Response> createSellerResource(SellerRequest sellerRequest){
+
+    public static String addProductToSellerGetId(String sellerId){
+       Response productAdded = createProductResource(createValidProduct(), sellerId);
+       return extractLocationId(productAdded);
+    }
+
+    public static Response createSellerResource(SellerRequest sellerRequest){
         return createResource("/sellers", sellerRequest);
     }
 
-    public static ProductRequest createProduct() {
+    public static Response getSellerById(String sellerId) throws JsonProcessingException {
+        Response sellerResponse = getResourceById("/sellers/{sellerId}", sellerId);
+        return sellerResponse;
+}
+
+    public static ProductRequest createValidProduct() {
         ProductRequest productRequest = new ProductRequest();
         productRequest.title = "My Product";
         productRequest.description = "Best product in the world";
@@ -67,23 +80,34 @@ public class End2EndUtils {
         return productRequest;
     }
 
-    public static ExtractableResponse<Response> createProductResource(ProductRequest productRequest, String sellerId){
+    public static Response createProductResource(ProductRequest productRequest, String sellerId){
         Header productSeller = new Header(SELLER_HEADER_NAME, sellerId);
         return createResource("/products", productSeller, productRequest);
     }
 
+    public static String createProductGetId(){
+        ProductRequest productRequest = createValidProduct();
+        Response response = createProductResource(productRequest, createSellerGetId());
+        return extractLocationId(response);
+    }
 
+    public static Response getProductById(String productId) throws JsonProcessingException {
+        Response productResponse = getResourceById("/products/{productId}", productId);
 
-    public static ExtractableResponse<Response> createResource(String path, Object bodyPayload) {
+        return productResponse;
+    }
+
+    public static Response createResource(String path, Object bodyPayload) {
         return given()
                 .contentType(ContentType.JSON)
                 .body(bodyPayload)
                 .when()
                 .post(path)
                 .then()
-                .extract();
+                .extract()
+                .response();
     }
-    private static ExtractableResponse<Response> createResource(String path, Header header, Object bodyPayload) {
+    private static Response createResource(String path, Header header, Object bodyPayload) {
         return given()
                 .contentType(ContentType.JSON)
                 .header(header)
@@ -91,21 +115,24 @@ public class End2EndUtils {
                 .when()
                 .post(path)
                 .then()
-                .extract();
+                .extract()
+                .response();
     }
 
-    private static ExtractableResponse<Response> getResource(String locationHeader) {
+    private static Response getResourceById(String pathParam, String resourceId) {
         return given()
                 .contentType(ContentType.JSON)
                 .when()
-                .get(locationHeader)
+                .get(pathParam, resourceId)
                 .then()
-                .extract();
+                .extract()
+                .response();
     }
 
-    public static String extractLocationId(ExtractableResponse<Response> response){
-        String location = response.header("Location");
+    public static String extractLocationId(Response response){
+        String location = response.path("id");
         return location.substring(location.lastIndexOf("/") + 1);
     }
+
     }
 
