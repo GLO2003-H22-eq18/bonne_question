74303b6a8200bcb4c79b851ef35a668216916f82
Simon <simonhoude2406@gmail.com>
2022-03-21 17:30:33 -0400

    Create offer missing params exceptions

---
+++ src/main/java/ulaval/glo2003/product/exceptions/offers/MissingOfferMessageException.java
@@ -0,0 +1,7 @@
+package ulaval.glo2003.product.exceptions.offers;
+
+import ulaval.glo2003.exceptions.MissingArgumentException;
+
+public class MissingOfferMessageException extends MissingArgumentException {
+    public MissingOfferMessageException() {super ("Offer message is missing");}
+}
---
+++ src/main/java/ulaval/glo2003/product/exceptions/offers/MissingOfferPhoneNumberException.java
@@ -0,0 +1,7 @@
+package ulaval.glo2003.product.exceptions.offers;
+
+import ulaval.glo2003.exceptions.MissingArgumentException;
+
+public class MissingOfferPhoneNumberException extends MissingArgumentException {
+    public MissingOfferPhoneNumberException() {super("Offer phone number is missing");}
+}
