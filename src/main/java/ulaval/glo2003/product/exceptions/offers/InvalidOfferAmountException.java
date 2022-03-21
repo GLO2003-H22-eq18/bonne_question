05d2ca7b3a960c61cfa53c87445e98ee0f995618
Simon <simonhoude2406@gmail.com>
2022-03-21 17:21:58 -0400

    Create invalid offer exceptions

---
+++ src/main/java/ulaval/glo2003/product/exceptions/offers/InvalidOfferAmountException.java
@@ -0,0 +1,7 @@
+package ulaval.glo2003.product.exceptions.offers;
+
+import ulaval.glo2003.exceptions.InvalidArgumentException;
+
+public class InvalidOfferAmountException extends InvalidArgumentException {
+    public InvalidOfferAmountException() {super("Offer amount is invalid");}
+}
