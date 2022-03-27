7de1c9906b44f14efa3ce1e79c82ddc03e6d58c1
Nathan Paquet <NAPAQ83@ulaval.ca>
2022-03-27 15:05:34 -0400

    Create buyer assembler and response

---
+++ src/main/java/ulaval/glo2003/product/ui/responses/BuyerResponse.java
@@ -0,0 +1,15 @@
+package ulaval.glo2003.product.ui.responses;
+
+public class BuyerResponse {
+    public String name;
+    public String email;
+    public String phoneNumber;
+
+    public BuyerResponse(String name,
+                         String email,
+                         String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
