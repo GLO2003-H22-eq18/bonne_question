09de7e7a4c71b1a010601e8c4ecf16d462f67c0e
Nathan Paquet <NAPAQ83@ulaval.ca>
2022-04-02 21:15:03 -0400

    Add ObjectId in BuyerAssemblerTest

--- src/test/java/ulaval/glo2003/unit/BuyerAssemblerTest.java
+++ src/test/java/ulaval/glo2003/unit/BuyerAssemblerTest.java
@@ -1,42 +1,40 @@
 package ulaval.glo2003.unit;
 
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import ulaval.glo2003.product.domain.Offer;
-import ulaval.glo2003.product.ui.assemblers.OfferAssembler;
+import ulaval.glo2003.product.ui.assemblers.BuyerAssembler;
 import ulaval.glo2003.product.ui.responses.BuyerResponse;
 
 import static com.google.common.truth.Truth.assertThat;
 
 public class BuyerAssemblerTest {
 
-    private static OfferAssembler offerAssembler;
+    private static BuyerAssembler buyerAssembler;
 
     @BeforeAll
-    public static void setup() { offerAssembler = new OfferAssembler();}
-
-    public Offer getOffer() {
-        Double amount = 25.0;
-        String message = "Significant message";
-        String id = "1";
-        String name = "John";
-        String email = "sickmail@hotmail.com";
-        String phoneNumber = "5989782222";
-
-        return new Offer(id, amount, message, name ,email ,phoneNumber);
-    }
+    public static void setup() { buyerAssembler = new BuyerAssembler();}
 
     @Test
     void givenOffer_whenCreateOfferResponse_thenCorrectOfferResponse() {
         Offer offer = getOffer();
 
-        BuyerResponse buyerResponse = new BuyerResponse(
-                "John",
-                "sickmail@hotmail.com",
-                "5989782222");
+        BuyerResponse buyerResponse = buyerAssembler.createBuyerResponse(offer);
 
         assertThat(buyerResponse.name).isEqualTo(offer.getName());
         assertThat(buyerResponse.email).isEqualTo(offer.getEmail());
         assertThat(buyerResponse.phoneNumber).isEqualTo(offer.getPhoneNumber());
+    }
+
+    public Offer getOffer() {
+        Double amount = 25.0;
+        String message = "Significant message";
+        ObjectId id = new ObjectId();
+        String name = "John";
+        String email = "sickmail@hotmail.com";
+        String phoneNumber = "5989782222";
+
+        return new Offer(id, amount, message, name ,email ,phoneNumber);
     }
 }
