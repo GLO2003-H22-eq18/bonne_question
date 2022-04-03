08dd09c3dd1534398e6710b607c5cc46838f1ea3
Nathan Paquet <NAPAQ83@ulaval.ca>
2022-04-02 21:10:33 -0400

    Changed OfferAssemblerTest with objectId

--- src/test/java/ulaval/glo2003/unit/OfferAssemblerTest.java
+++ src/test/java/ulaval/glo2003/unit/OfferAssemblerTest.java
@@ -1,12 +1,11 @@
 package ulaval.glo2003.unit;
 
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ulaval.glo2003.product.domain.Offer;
 import ulaval.glo2003.product.ui.assemblers.OfferAssembler;
 import ulaval.glo2003.product.ui.responses.OfferResponse;
-
-import java.time.OffsetDateTime;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -14,24 +13,8 @@ class OfferAssemblerTest {
 
     private static OfferAssembler offerAssembler;
 
-   /* private static final Double AMOUNT = 25.0;
-    private static final String MESSAGE = "Significant message";
-    private static final Double INVALID_AMOUNT = null;
-    private static final String INVALID_MESSAGE = "  \n\t";*/
-
     @BeforeAll
     public static void setup() { offerAssembler = new OfferAssembler();}
-
-    public Offer getOffer() {
-        Double amount = 25.0;
-        String message = "Significant message";
-        String id = "1";
-        String name = "John";
-        String email = "sickmail@hotmail.com";
-        String phoneNumber = "5989782222";
-
-        return new Offer(id, amount, message, name ,email, phoneNumber);
-    }
 
     @Test
     void givenOffer_whenCreateOfferResponse_thenCorrectOfferResponse() {
@@ -45,5 +28,16 @@ class OfferAssemblerTest {
         assertThat(offerResponse.buyer.name).isEqualTo(offer.getName());
         assertThat(offerResponse.buyer.email).isEqualTo(offer.getEmail());
         assertThat(offerResponse.buyer.phoneNumber).isEqualTo(offer.getPhoneNumber());
+    }
+
+    public Offer getOffer() {
+        Double amount = 25.0;
+        String message = "Significant message";
+        ObjectId id = new ObjectId();
+        String name = "John";
+        String email = "sickmail@hotmail.com";
+        String phoneNumber = "5989782222";
+
+        return new Offer(id, amount, message, name ,email, phoneNumber);
     }
 }
